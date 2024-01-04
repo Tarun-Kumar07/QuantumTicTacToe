c8aabe941afbe56b4a555126eea39ce27bdc29bd
Tarun-Kumar07 <tarunkumar00137@gmail.com>
2024-01-04 10:19:45 +0530

    Use requirements.txt for dependencies

--- setup.py
+++ setup.py
@@ -6,11 +6,4 @@ setup(
     version="1.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    install_requires=[
-        "black==23.12.1",
-        "pytest==7.4.3",
-        "qiskit==0.45.1",
-        "qiskit-aer==0.13.1",
-        "streamlit==1.29.0",
-    ],
 )
